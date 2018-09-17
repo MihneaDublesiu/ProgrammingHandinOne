b0aa0e577ba01dc8d980aa8f881c833e32764bf0
MihneaDublesiu <mihneadublesiu@yahoo.com>
2018-09-17 16:19:45 +0200

    Programming

---
+++ Classes/Customer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerRetrieval
+{
+    public class Customer
+    {
+        public string Name; 
+        public int Age; 
+        public int Zip; 
+        public string Password; 
+
+       public Customer()
+        {
+
+        }
+
+        public Customer (string name)
+            
+        {
+            this.Name = name; 
+        }
+
+        public Customer (string name, int age)
+          
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public Customer (string name, int age, int zip)
+             
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Zip = zip;
+        }
+
+        public Customer (string name, int age, int zip, string password)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Zip = zip;
+            this.Password = password;
+        }  
+        
+            
+
+        public override string ToString()
+        {
+            return "\nName:" + Name + " \nAge: " + Age + " \nZip Code: " + Zip + " \nPassword: " + Password + "\n";
+        }
+
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerRetrieval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Press any key to display the customer information.");
+            Console.ReadKey();
+            var customer = new Customer(); 
+            customer.Name = "John Scarce";
+            customer.Age = 25;
+            customer.Zip = 4389;
+            customer.Password = "thisisthepassword";
+            Console.WriteLine(customer.ToString());
+            
+
+        }
+              
+    }
+}
+
+
